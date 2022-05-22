3c88bd632c4a088cc494187bacd349c549a4b7bc
MaksymKundeev <mkundeev@gmail.com>
2022-05-22 14:00:03 +0300

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+
+import Notiflix from 'notiflix';
+
+const refs= {
+  firstDelay: document.querySelector('[name="delay"]'),
+  delayStep: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', onSubmit)
+
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const amount = Number(refs.amount.value);
+  const delayStep = Number(refs.delayStep.value);
+  let delay=Number(refs.firstDelay.value);
+    for (let i = 0; i < amount; i += 1) {
+      
+      createPromise(i + 1, delay)
+        .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay}) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      delay += delayStep;
+    }
+  
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      }
+      rejected({ position, delay })
+    }, delay)
+  });
 }
+
